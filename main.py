4cb2c2b6abfb0243a31652334c736d0e66ffda6b
GJIIB <glebnazarov.2008@gmail.com>
2024-09-12 20:39:32 +0300

    Без змін

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
 
 
-class MainApp(MDApp):
+class WeatherScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class LCloudApp(MDApp):
     def build(self):
+        Builder.load_file('style.kv')
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Orange"
 
@@ -18,4 +24,4 @@ class MainApp(MDApp):
         )
 
 
-MainApp().run()
+LCloudApp().run()
