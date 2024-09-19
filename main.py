0642f63210af543a2e46836f7e280d2a64b528ac
GJIIB <glebnazarov.2008@gmail.com>
2024-09-19 20:33:33 +0300

    Деякі зміни інтерфейсу

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
 
 
+
+
 class WeatherScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -12,16 +14,9 @@ class LCloudApp(MDApp):
     def build(self):
         Builder.load_file('style.kv')
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Orange"
+        self.theme_cls.primary_palette = "Purple"
 
-        return (
-            MDScreen(
-                MDRectangleFlatButton(
-                    text="Hello, World",
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                )
-            )
-        )
+        return WeatherScreen()
 
 
 LCloudApp().run()
